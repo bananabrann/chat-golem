ba50952195ff5de2e3387913e84cb39486b6fa8b
Lee <plbrannan95@gmail.com>
2021-09-08 08:04:47 -0400

    Bot not responds to and saves DMs

---
+++ src/classes/DirectMessage.ts
@@ -0,0 +1,29 @@
+import { Message } from "discord.js";
+import fs from "fs";
+import moment from "moment";
+import { v4 as uuidv4 } from "uuid";
+
+export default class DirectMessage {
+  // prettier-ignore
+  public static defaultResponse: string = "Hey there, I see you messaged me, but unfortunately I'm not programmed to do anything to your DMs.\nI've saved your message, but do not count on a speedy reply. If something is an emergency, please reach out to someone else.";
+
+  public static captureMessage(message: Message) {
+    const uuid: string = uuidv4();
+    // prettier-ignore
+    process.stdout.write(`[${moment()}] ${message.author.tag} sent a DM, capturing it to ${uuid.slice(-4)}... `);
+
+    fs.writeFile(
+      `res/dms/${uuidv4()}.dm.txt`,
+      message.content,
+      "utf8",
+      (error: unknown) => {
+        if (error) {
+          console.error(error);
+          process.stdout.write("ERROR\n");
+        } else {
+          process.stdout.write("OK\n");
+        }
+      }
+    );
+  }
+}
